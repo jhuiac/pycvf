e94e061cf5d9199918c4284ff6a3afcbf7ff7a41
bertrand.nouvel <bertrand.nouvel@705524ac-de69-11de-9f73-7335da764322>
2010-02-09 09:51:26 +0000

    bugfix on install procedure for ubuntu

--- pycvf/trunk/pycvf/maintenance/pycvf_modify_bashrc.py
+++ pycvf/trunk/pycvf/maintenance/pycvf_modify_bashrc.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os,sys
+import os,sys,re
 
 def compute_platform_id():
   if sys.platform in [ "win32","win64"]:
@@ -10,7 +10,7 @@ def compute_platform_id():
 def modify_bashrc(pycvfdir=None):
   xfile=__file__
   if (xfile[0]!=os.path.sep):
-    xfile=os.path.join(xfile)
+    xfile=os.path.join(os.getcwd(),xfile)
   if pycvfdir==None:
     pycvfdir=os.path.dirname(os.path.dirname(os.path.dirname(xfile)))
   assert(pycvfdir!="")
@@ -43,7 +43,7 @@ def touch_pycvf_settings_py():
     f=open(os.path.join(os.environ["HOME"],".pycvf-settings.py"),"a")
     f.close()
   
-if __name__="__main__":
+if __name__=="__main__":
   modify_bashrc()
   touch_pycvf_settings_py()
   
